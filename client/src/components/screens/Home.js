af642504389de5b342186c3118f6c2face12c2ef
FacuCuzziol <facucuzziol@gmail.com>
2020-07-09 18:11:48 -0300

    first changes in Home screen

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -2,7 +2,46 @@ import React from 'react'
 
 const Home = () =>{
     return(
-        <h1>Home</h1>
+        <div className="home">
+            <div className="card home-card">
+                <h5>Jefferson Gutierritoz</h5>
+                <div className="card-image">
+                    <img src="https://www.jeep.com//content/dam/fca-brands/na/jeep/en_us/2020/wrangler/gallery/exterior/Gallery-Exterior-White-Colorado-Sunset-Desktop.jpg"/>
+                </div>
+                <div className="card-content">
+                <i className="material-icons">favorite</i>
+                    <h6>La Jeepeta</h6>
+                    <p>Aca con mi nueva jeepeta breeeeeo</p>
+                    <input type="text" placeholder="add a comment"/>
+                </div>
+            </div>
+            <div className="card home-card">
+                <h5>Jefferson Gutierritoz</h5>
+                <div className="card-image">
+                    <img src="https://www.jeep.com//content/dam/fca-brands/na/jeep/en_us/2020/wrangler/gallery/exterior/Gallery-Exterior-White-Colorado-Sunset-Desktop.jpg"/>
+                </div>
+                <div className="card-content">
+                <i className="material-icons">favorite</i>
+                    <h6>La Jeepeta</h6>
+                    <p>Aca con mi nueva jeepeta breeeeeo</p>
+                    <input type="text" placeholder="add a comment"/>
+                </div>
+            </div>
+            <div className="card home-card">
+                <h5>Jefferson Gutierritoz</h5>
+                <div className="card-image">
+                    <img src="https://www.jeep.com//content/dam/fca-brands/na/jeep/en_us/2020/wrangler/gallery/exterior/Gallery-Exterior-White-Colorado-Sunset-Desktop.jpg"/>
+                </div>
+                <div className="card-content">
+                <i className="material-icons">favorite</i>
+                    <h6>La Jeepeta</h6>
+                    <p>Aca con mi nueva jeepeta breeeeeo</p>
+                    <input type="text" placeholder="add a comment"/>
+                </div>
+            </div>
+        </div>
+
+        
     )
 }
 
