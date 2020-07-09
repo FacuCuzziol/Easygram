367bdbee9acbdea9d2f4a73b5cff3f92a49b413a
FacuCuzziol <facucuzziol@gmail.com>
2020-07-09 16:28:40 -0300

    adding basic frontend structure and navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+const NavBar = () =>{
+    return(
+        <nav>
+        <div className="nav-wrapper blue" >
+          <Link to="/" className="brand-logo left">Easygram</Link>
+          <ul id="nav-mobile" className="right">
+            <li><Link to="/signin">SignIn</Link></li>
+            <li><Link to="/signup">SignUp</Link></li>
+            <li><Link to="/profile">Profile</Link></li>
+          </ul>
+        </div>
+      </nav>
+    )
+}
+
+export default NavBar
