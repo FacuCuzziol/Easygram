ea782707aecca5d5bd3cdcf688fad09cc8d13ee8
FacuCuzziol <facucuzziol@gmail.com>
2020-07-10 20:32:12 -0300

    fetching posts and showing real content on Home page

--- server/routes/post.js
+++ server/routes/post.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const PostController = require('../controllers/PostController')
 const requireLogin = require('../middleware/requireLogin');
 router.post('/createpost',requireLogin,PostController.createpost)
-router.get('/allpost',PostController.getallposts);
+router.get('/allpost',requireLogin,PostController.getallposts);
 router.get('/mypost',requireLogin,PostController.getmypost);
 
 module.exports = router
