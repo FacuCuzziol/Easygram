16afbd3bd4f28e601b3086a09379216ca23f33c1
FacuCuzziol <facucuzziol@gmail.com>
2020-07-10 19:55:16 -0300

    more client logic

--- client/src/components/screens/SignIn.js
+++ client/src/components/screens/SignIn.js
@@ -1,9 +1,10 @@
-import React,{useState} from 'react'
+import React,{useState,useContext} from 'react'
 import {Link,useHistory} from 'react-router-dom'
 import M from 'materialize-css'
+import {UserContext} from '../../App'
 const SignIn = () =>{
-    const history = useHistory()
-
+    const history = useHistory(UserContext);
+    const {state,dispatch} = useContext
     const [password,setPassword] = useState("")
     const [email,setEmail] = useState("")
     const PostData =()=>{
@@ -29,6 +30,7 @@ const SignIn = () =>{
             else{
                 localStorage.setItem("jwt",data.token)
                 localStorage.setItem("user",JSON.stringify(data.user))
+                dispatch({type:"USER",payload:data.user})
                 M.toast({html:"sign In success",classes:"#66bb6a green lighten-1"})
                 history.push('/')
             }
