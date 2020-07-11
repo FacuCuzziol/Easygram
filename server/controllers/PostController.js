11e2eadcc5e02a5925a78d746421dfcb38cbfbbe
FacuCuzziol <facucuzziol@gmail.com>
2020-07-11 18:29:23 -0300

    adding delete post featue

--- server/controllers/PostController.js
+++ server/controllers/PostController.js
@@ -98,3 +98,21 @@ exports.comment = (req,res,next) =>{
             }
         })
 }
+
+exports.deletePost = (req,res,next)=>{
+    Post.findOne({_id:req.params.postId})
+    .populate("postedBy","_id")
+    .exec((err,post)=>{
+        if(err || !post){
+            return res.status(422).json({error:err})
+        }
+        if(post.postedBy._id.toString() === req.user._id.toString()){
+            post.remove()
+            .then(result=>{
+                res.json(result)
+            }).catch(err=>{
+                console.log(err)
+            })
+        }
+    })
+}
--- server/routes/post.js
+++ server/routes/post.js
@@ -8,5 +8,5 @@ router.get('/mypost',requireLogin,PostController.getmypost);
 router.put('/like',requireLogin,PostController.likePost)
 router.put('/unlike',requireLogin,PostController.unlikePost)
 router.put('/comment',requireLogin,PostController.comment)
-
+router.delete('/deletepost/:postId',requireLogin,PostController.deletePost)
 module.exports = router
