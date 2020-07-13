6a57ea7cab2aed5ac8f10a1ed7a17b96b98c0be6
FacuCuzziol <facucuzziol@gmail.com>
2020-07-13 11:31:19 -0300

    rechanging structure

--- server/app.js
+++ server/app.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const PORT = process.env.PORT || 5000;
-const {MONGOURI} = require('./keys');
+const PORT = 5000;
+const {MONGO_URI} = require('./keys');
 const cors = require('cors')
-const path = require('path')
 app.use(cors())
 
 
 
 
-mongoose.connect(process.env.MONGODB_URI || MONGOURI,{
+mongoose.connect(MONGO_URI,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 });
@@ -26,14 +25,6 @@ app.use(express.json());
 app.use(require('./routes/auth'));
 app.use(require('./routes/post'));
 app.use(require('./routes/user'));
-
-
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
-    app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname,'client','build','index.html')); //relative path
-    })
-}
 
 app.listen(PORT,()=>{
     console.log("server is running on",PORT);
